ecfcd64d418e00c03deb16a92f193981e7808091
Peter McNaught <petermmcnaught@gmail.com>
2018-07-05 16:31:15 +1000

    hooked in mongo db

--- schema/types/me.js
+++ schema/types/me.js
@@ -3,11 +3,13 @@ const {
     GraphQLID,
     GraphQLString,
     GraphQLNonNull,
-    GraphQLList
-} = require('graphql')
+    GraphQLList,
+    GraphQLInt
+  } = require('graphql');
 
-const ContestType = require('./contest');
 const pgdb = require('../../database/pgdb');
+const mdb = require('../../database/mdb');
+const ContestType = require('./contest');
 
 module.exports = new GraphQLObjectType({
     name: 'MeType',
@@ -18,18 +20,33 @@ module.exports = new GraphQLObjectType({
         lastName: { type: GraphQLString },
         fullName: {
             type: GraphQLString,
-            resolve: obj => `${obj.firstName} + ${obj.lastName}`
+            resolve: obj => `${obj.firstName} ${obj.lastName}`
         },
         email: { type: new GraphQLNonNull(GraphQLString) },
         createdAt: { type: GraphQLString },
         contests: {
             type: new GraphQLList(ContestType),
-            resolve(obj, args, {pgPool}) {
+            resolve(obj, args, { pgPool }) {
                 return pgdb(pgPool).getContests(obj);
+            }
+        },
+        contestsCount: {
+            type: GraphQLInt,
+            resolve(obj, args, { mPool }, { fieldName }) {
+                return mdb(mPool).getCounts(obj, fieldName);
+            }
+        },
+        namesCount: {
+            type: GraphQLInt,
+            resolve(obj, args, { mPool }, { fieldName }) {
+                return mdb(mPool).getCounts(obj, fieldName);
+            }
+        },
+        votesCount: {
+            type: GraphQLInt,
+            resolve(obj, args, { mPool }, { fieldName }) {
+                return mdb(mPool).getCounts(obj, fieldName);
             }
         }
     }
 });
-
-
-
